76ec028abda11be365222b1a3975cabbdc10448e
poofee <poofee@qq.com>
2018-01-31 11:36:57 +0800

    给网格留出了空白，准备绘制坐标系

--- feem/pf_graphicview.cpp
+++ feem/pf_graphicview.cpp
@@ -3,19 +3,21 @@
 #include <QPainter>
 #include <QtMath>
 
-PF_GraphicView::PF_GraphicView(QWidget *parent) : QWidget(parent)
+PF_GraphicView::PF_GraphicView(QWidget *parent)
+    : QWidget(parent)
+    ,PixmapLayer1(nullptr)
+    ,PixmapLayer2(nullptr)
+    ,PixmapLayer3(nullptr)
 {
-    PixmapLayer1 = nullptr;
-    PixmapLayer2 = nullptr;
-    PixmapLayer3 = nullptr;
+
 }
 
 void PF_GraphicView::paintEvent(QPaintEvent *){   
     getPixmapForView(&PixmapLayer1);
-    PixmapLayer1->fill(QColor(0,0,0));
+    PixmapLayer1->fill(QColor(250,250,250,80));
 
     QPainter painter1(PixmapLayer1);
-    painter1.setPen(QColor(255,255,255));
+    painter1.setPen(QColor(232,241,246));
     drawLayer1(&painter1);
     painter1.end();
 
@@ -91,17 +93,26 @@ void PF_GraphicView::resizeEvent(QResizeEvent *e)
 
 void PF_GraphicView::drawLayer1(QPainter * painter){
 
-    for(int i = 0; i < this->width();i+=10){
-        for(int j = 0; j < this->height();j+=10){
+    for(int i = 0+50; i < this->width()-50;i+=10){
+        for(int j = 0+50; j < this->height()-50;j+=10){
             painter->drawPoint(i,j);
         }
     }
     painter->setPen(Qt::gray);
-    for(int i = 0; i < this->width();i+=200){
-          painter->drawLine(QPointF(i,0),QPointF(i,height()));
+    int numgridw = 10;
+    int numgridh = 5;
+    int leftmargin = 50;
+    int rightmargin = 50;
+    int topmargin = 50;
+    int bottommargin = 50;
+    int spaceW = (width()-rightmargin-leftmargin)/numgridw;
+    int spaceH = (height()-topmargin-bottommargin)/numgridh;
+
+    for(int i = 0; i <= numgridw;i+=1){
+        painter->drawLine(QPointF(i*spaceW+leftmargin,0+topmargin),QPointF(i*spaceW+leftmargin,spaceH*numgridh+topmargin));
     }
-    for(int i = 0; i < this->height();i+=200){
-          painter->drawLine(QPointF(0,i),QPointF(width(),i));
+    for(int i = 0; i <= numgridh;i+=1){
+        painter->drawLine(QPointF(0+leftmargin,i*spaceH+topmargin),QPointF(spaceW*numgridw+leftmargin,i*spaceH+topmargin));
     }
 }
 
